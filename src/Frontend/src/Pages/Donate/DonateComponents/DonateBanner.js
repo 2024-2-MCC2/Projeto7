ef5844d6fe7b08bfa6e82bce9850e4e2c0259b7f
Felipewk7 <163530061+Felipewk7@users.noreply.github.com>
2024-11-05 07:08:43 -0600

    Style adjustment

--- src/Frontend/src/Pages/Donate/DonateComponents/DonateBanner.js
+++ src/Frontend/src/Pages/Donate/DonateComponents/DonateBanner.js
@@ -1,48 +1,63 @@
 import React from 'react';
 import DonateBannerImagem from '../../../assets/img/Donate-banner.jpg'; 
+import styled from 'styled-components';
 
 const Donate = () => {
-  const styles = {
-    container: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      backgroundColor: '#ccffcc',
-      padding: '20px',
-      borderRadius: '8px',
-      maxWidth: '1500px',
-      margin: '20px auto',
-    },
-    title: {
-      fontSize: '4em',
-      color: '#004c3f',
-      margin: 0,
-      left:0,
-    },
-    SubTitle: {
-fontSize:'0,2em',
-color:'#004c3f',
-    },
-    image: {
-      width: '800px',
-      height: '400px',
-      objectFit: 'cover',
-      backgroundColor: '#ddd',
-      borderRadius: '8px',
-    },
-  };
-
   return (
-    <div style={styles.container}>
-      <div className="donate-text">
-        <h2 style={styles.title}>Doe Agora</h2>
-        <h2 style={styles.SubTitle}>Reconstrua casas e transforme vidas</h2>
-      </div>
-      <div className="donate-image">
-        <img src={DonateBannerImagem} alt="Imagem representativa de doação" style={styles.image} />
-      </div>
-    </div>
+    <Container>
+      <DonateText>
+        <Title>Doe Agora</Title>
+        <SubTitle>Reconstrua casas e transforme vidas</SubTitle>
+      </DonateText>
+      <DonateImage>
+        <img src={DonateBannerImagem} alt="Imagem representativa de doação" />
+      </DonateImage>
+    </Container>
   );
-}
+};
 
 export default Donate;
+
+// Styled Components
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #ccffcc;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 1900px;
+  margin: 20px auto;
+`;
+
+const DonateText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Title = styled.h2`
+  font-size: 4em;
+  color: #004c3f;
+  margin: 0;
+`;
+
+const SubTitle = styled.h2`
+  font-size: 1.5em; /* Corrigido o valor do fontSize */
+  color: #004c3f;
+`;
+
+const DonateImage = styled.div`
+  width: 800px;
+  height: 400px;
+  overflow: hidden;
+  border-radius: 8px;
+  
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    background-color: #ddd;
+  }
+`;
