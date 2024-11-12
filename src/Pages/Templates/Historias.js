cfcc9a6e9e6e10585ecffd724bf04ac9e87d98ec
t_rident <gabrieldavanco4@gmail.com>
2024-11-12 10:59:54 -0300

    update de pastas (dnv)

--- src/Pages/Templates/Historias.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Header from "../../ComponentsGerais/Header";
-
-function Historias (){
-
-
-}
-export default Historias;
--- src/Pages/Templates/TomeAcao.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-
-function TomeAcaoContainer(){
-    return(
-        <h3>gayo</h3>
-    )
-}
-export default TomeAcaoContainer
