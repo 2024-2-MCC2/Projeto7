f3eb7fcef72e52df2ea38e46991302316e9432b5
t_rident <gabrieldavanco4@gmail.com>
2024-11-12 08:51:43 -0300

    Atualização da logo do header

--- src/Frontend/src/ComponentsGerais/Header.js
+++ src/Frontend/src/ComponentsGerais/Header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import Logo from '../assets/img/Logo.png';
+import Logo from '../assets/img/headerlogobranca.png'
 import styled from 'styled-components';
 import ScrollToTop from './ScrollToTop';
 
